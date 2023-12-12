f8150583821a99efcae6ca6240e2f266fce17769
johnnataa <johnnata_@outlook.com>
2023-12-11 21:06:19 -0300

    Att 2 Atividades dia 11

--- src/atividades_dia_11/Atividade2.java
+++ src/atividades_dia_11/Atividade2.java
@@ -10,14 +10,31 @@ public class Atividade2 {
 		
 		Scanner leia = new Scanner (System.in);
 		
+		int soma = 0;
+		float media = 0;
 		int vetorInteiros[] = new int[10];
 		
 		for (int indice = 0; indice < 10; indice++) {
 			System.out.println("Digite o " + (indice + 1) + "° número: ");
 			vetorInteiros[indice] = leia.nextInt();
+			soma += vetorInteiros[indice];
+			media = soma / 10;
 		}
-		System.out.println("Elementos nos índices ímpares: " + vetorInteiros[1] + " " + vetorInteiros[3] + " " + vetorInteiros[5] + " " + vetorInteiros[7] + " " + vetorInteiros[9]);
-
+		
+		System.out.println("Elementos nos índices ímpares: ");
+		for (int indice = 0; indice < 10; indice++) {
+			if (indice % 2 == 1) {
+				System.out.print(vetorInteiros[indice] + " ");
+			}
+		}
+		
+		System.out.println("\n\nElementos pares: ");
+		for (int indice = 0; indice < 10; indice++) {
+			if (vetorInteiros[indice] % 2 == 0) {
+				System.out.print(vetorInteiros[indice] + " ");
+			}
+}
+		System.out.println("\nSoma: " + soma);
+		System.out.println("\nMédia: " + media);
 	}
-
 }
